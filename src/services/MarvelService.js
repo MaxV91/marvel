70496f1da342b9bb5f1a29e44a9e8fb1decc8e84
Maxim K <max.kotsurenko@gmail.com>
2021-11-10 19:54:18 +0300

    update service

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,6 @@
-
-
 class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=2e8408ff696a16898c434c6a2c488ef7';
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -12,12 +12,26 @@ class MarvelService {
     return await res.json();
   }
 
-  getAllCharacters = () => {
-    return this.getResource('https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=c5d6fc8b83116d92ed468ce36bac6c62');
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter)
   }
 
-  getCharacter = (id) => {
-    return this.getResource(`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=c5d6fc8b83116d92ed468ce36bac6c62`)
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+    return this._transformCharacter(res.data.results[0]);
+  }
+
+  _transformCharacter = (char) => {
+    return {
+      id: char.id,
+      name: char.name,
+      description: char.description,
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      comics: char.comics.items
+    }
   }
 }
 
