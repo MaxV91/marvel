4e39beda2ca09e9df13f592c3186af3f1a6cd003
Maxim K <max.kotsurenko@gmail.com>
2021-11-19 09:37:42 +0300

    refactor App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,37 +1,39 @@
-import {Component} from "react";
+import {Component, useState} from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
-
 import CharInfo from "../charInfo/CharInfo";
 import decoration from '../../resources/img/vision.png';
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
+import {MainPage, ComicsPage, Page404, SingleComicPage} from '../../pages';
 
-class App extends Component {
-  state = {
-    selectedChar: null
-  }
-
-  onCharSelected = (id) => {
-    this.setState({
-      selectedChar: id
-    })
-  }
-
-  render() {
-    return (
+const App = () => {
+  return (
+    <Router>
       <div className="app">
         <AppHeader/>
         <main>
-          <RandomChar/>
-          <div className="char__content">
-            <CharList onCharSelected={this.onCharSelected}/>
-            <CharInfo charId={this.state.selectedChar}/>
-          </div>
-          <img className="bg-decoration" src={decoration} alt="vision"/>
+          <Switch>
+            <Route exact path="/">
+              <MainPage />
+            </Route>
+            <Route exact path="/comics">
+              <ComicsPage />
+            </Route>
+            <Route exact path="/comics/:comicId">
+              <SingleComicPage />
+            </Route>
+            <Route path="*">
+              <Page404 />
+            </Route>
+          </Switch>
         </main>
       </div>
-    )
-  }
+    </Router>
+  )
 }
 
 export default App;
