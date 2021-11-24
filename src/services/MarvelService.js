ec1e44c96d1a0bb9c14998f2122d799d06b757c0
Maxim K <max.kotsurenko@gmail.com>
2021-11-24 10:31:10 +0300

    refactor MarvelService

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -16,6 +16,16 @@ const useMarvelService = () => {
     return _transformCharacter(res.data.results[0]);
   }
 
+  const getAllComics = async (offset = 0) => {
+    const res = await request(`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`);
+    return res.data.results.map(_transformComics);
+  }
+
+  const getComic = async (id) => {
+    const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+    return _transformComics(res.data.results[0]);
+  }
+
   const _transformCharacter = (char) => {
     return {
       id: char.id,
@@ -28,7 +38,19 @@ const useMarvelService = () => {
     }
   }
 
-  return { loading, error, getAllCharacters, getCharacter, clearError };
+  const _transformComics = (comics) => {
+    return {
+      id: comics.id,
+      title: comics.title,
+      description: comics.description,
+      pageCount: comics.pageCount,
+      thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
+      language: comics.textObjects.language || 'en-us',
+      price: comics.prices[0].price
+    }
+  }
+
+  return { loading, error, clearError, getAllCharacters, getCharacter, getAllComics, getComic };
 }
 
 export default useMarvelService;
